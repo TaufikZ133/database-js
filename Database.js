49b1a5f8a281c431233e02b173e2cd0c6aeb1bb0
TaufikZ133 <143265984+TaufikZ133@users.noreply.github.com>
2023-11-21 23:03:34 +0700

    Update Database.js

--- Database.js
+++ Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db','root','',{
-    password: '',
-    host: 'localhost'
+const db = new Sequelize('auth_db','srv442907','',{
+    host: '178.16.143.160',
+    dialect: 'mysql'
 });
 
 export default db;
